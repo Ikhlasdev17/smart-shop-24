b49a94470211a830f6c42e54264e56de1f0c2a42
Ikhlasdev17 <iklasaralbaev@gmail.com>
2022-10-21 16:17:31 +0500

    branches get/update/create completed

---
+++ src/Pages/Branches/Branches.jsx
@@ -0,0 +1,227 @@
+import { Select, DatePicker, Table, Skeleton, Button, Modal, Drawer, Form, Input, message } from 'antd'
+import React, { useState, useEffect } from 'react'
+import ModalAction from '../../components/ModalAction/ModalAction';
+
+import axios from 'axios';
+import { fetchingProducts, fetchedProducts } from '../../redux/productsSlice';
+
+import './Branches.scss';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setToken, URL } from '../../assets/api/URL'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
+import moment from 'moment'
+
+import { useTranslation } from 'react-i18next';
+
+const { RangePicker } = DatePicker;
+const { Option } = Select;
+
+const Branches = () => {
+  const dispatch = useDispatch()
+  const [branches, setBranches] = useState([])
+  const [loading, setLoading] = useState(true)
+  const { t } = useTranslation()
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [modalType, setModalType] = useState("")
+  const [newBranch, setNewBranch] = useState({
+    name: "",
+    is_main: false
+  })
+  const [selectedBranch, setSelectedBranch] = useState({})
+  const [refresh, setRefresh] = useState(false)
+
+  useEffect(() => {
+    if (modalType === "add") {
+      setNewBranch({
+        name: "",
+        is_main: false
+      })
+    } else if (modalType === "update") {
+      setNewBranch({
+        name: selectedBranch?.name,
+        is_main: selectedBranch?.is_main
+      })
+    }
+  }, [modalType, modalIsOpen])
+
+
+  useEffect(async () => {
+    setLoading(true)
+    const response = await
+      axios.get(`
+        ${URL}/api/branches`, setToken())
+
+    if (response.status === 200) {
+      setBranches(response.data.payload)
+      setLoading(false)
+    }
+
+  }, [refresh])
+
+
+
+  const dataSource = [];
+
+  branches?.map(item => {
+    dataSource.push({
+      key: item?.product_id,
+      branch: item?.name,
+      is_main: item?.is_main ? t("yes") : t("no"),
+      actions: (
+        <div className='table-button__group'>
+          <Button className="table-action" onClick={() => {
+            setModalType("update")
+            setSelectedBranch(item)
+            setModalIsOpen(true)
+          }}><i className="bx bx-edit"></i></Button>
+          {/* <Button className="table-action"><i className="bx bx-trash"></i></Button> */}
+        </div>
+      )
+    })
+  })
+
+  const columns = [
+    {
+      title: t('branches'),
+      dataIndex: 'branch',
+      key: 'key',
+      width: "70%"
+    },
+    {
+      title: t('main_branch'),
+      dataIndex: 'is_main',
+      key: 'key',
+    },
+    {
+      title: t("actions"),
+      dataIndex: "actions",
+      key: "actions"
+    }
+  ];
+
+  const handleSubmit = () => {
+    if (newBranch.name && newBranch.is_main !== null) {
+      if (modalType === "add") {
+        const index = branches.findIndex(item => item.name === newBranch.name)
+        if (index === -1){
+          axios.post(`${URL}/api/branches`, newBranch, setToken())
+            .then((res) => {
+              message.success(t("muaffaqiyatli"))
+              setModalIsOpen(false)
+              setNewBranch({ name: "", is_main: "" })
+              setRefresh(!refresh)
+            })
+            .catch((res) => {
+              setModalIsOpen(false)
+              setNewBranch({ name: "", is_main: "" })
+              message.error(t("xatolik"))
+            })
+        } else {
+          message.error(t("already_branch"))
+        }
+      } else if (modalType === "update") {
+        axios.patch(`${URL}/api/branches/${selectedBranch.id}`, newBranch, setToken())
+          .then((res) => {
+            message.success(t("muaffaqiyatli"))
+            setModalIsOpen(false)
+            setNewBranch({ name: "", is_main: "" })
+            setRefresh(!refresh)
+          })
+          .catch((res) => {
+            setModalIsOpen(false)
+            setNewBranch({ name: "", is_main: "" })
+            message.error(t("xatolik"))
+          })
+      }
+    }
+  }
+
+  const content = () => (
+    <Form layout='vertical' onFinish={handleSubmit} >
+      <Form.Item required label={t('branch_name')}>
+        <Input   required placeholder={t('branch_name')} className="form__input" onChange={(e) => {setNewBranch({...newBranch, name: e.target.value})}} value={newBranch.name} />
+      </Form.Item> 
+      <Form.Item required label={t('main_branch')}>
+        <Select required className='form__input' onChange={e => setNewBranch({ ...newBranch, is_main: e })} value={newBranch.is_main}>
+          <Select.Option value={true}>{t("yes")}</Select.Option>
+          <Select.Option value={false}>{t("no")}</Select.Option>
+        </Select>
+      </Form.Item> 
+      <Form.Item>
+          <Button htmlType='submit' className="btn btn-primary" style={{display: 'block'}}>
+              {t('save')}
+          </Button>
+      </Form.Item>
+    </Form>
+  )
+
+
+
+
+  return (
+
+    <div className="section main-page">
+      <h1 className="heading">{ t('main') }</h1>
+
+      {
+        modalIsOpen ? (
+          <Drawer
+            onClose={ () => setModalIsOpen(false) }
+            visible={ modalIsOpen }
+            title={ modalType === "add" ? t("create_branch") : t("update_branch") }
+          >
+            {content()}
+          </Drawer>
+        ) : null
+      }
+
+      <div className="content">
+        <div className="content-top">
+          <div className='content-top__group'></div>
+          <div className='content-top__group'>
+            <Button onClick={ () => {
+              setModalIsOpen(true)
+              setModalType("add")
+            } } className='btn btn-primary btn-md'>{ t("create_branch") }</Button>
+          </div>
+        </div>
+
+
+        <div className="content-body" >
+          <Skeleton loading={ loading } active>
+            <Table className="content-table lg-table" dataSource={ dataSource } columns={ columns } />
+
+            <div className="responsive__table">
+              { branches &&
+                branches.length > 0 ?
+                branches.map((item, index) => {
+                  return (
+                    <div
+                      className="responsive__table-item justify-between"
+                      key={ index }
+                    >
+                      <div className="responsive__table-item__details-name">
+                        <h3>{ item?.product_name }</h3>
+                      </div>
+
+                      <div className="responsive__table-item__details-count">
+                        <h3>{ item?.count }</h3>
+                      </div>
+
+
+                    </div>
+                  );
+                }) : null }
+            </div>
+
+          </Skeleton>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Branches
--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,269 +1,268 @@
-import { Select,DatePicker, Table, Input, Button, Form, Drawer, Skeleton, Modal } from 'antd'
-import React, { useState, useEffect } from 'react' 
+import { Select, DatePicker, Table, Input, Button, Form, Drawer, Skeleton, Modal } from 'antd'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import Content from './Content';
 
 import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 
-import {setToken, URL} from '../../assets/api/URL';
+import { setToken, URL } from '../../assets/api/URL';
 import { deleteCategory, fetchedCategories, fetchingCategories, fetchingErrorCategories } from '../../redux/categoriesSlice';
 import swal from 'sweetalert';
 
 import Swal from 'sweetalert2';
 
 
-import {DebounceInput} from 'react-debounce-input';
+import { DebounceInput } from 'react-debounce-input';
 
-const {Option} = Select;
+const { Option } = Select;
 const Categories = () => {
-    const [open, setOpen] = useState()
-    const dispatch = useDispatch()
-    const [loading, setLoading] = useState(true)
-    const {categories, categoriesFetchingStatus} = useSelector(state => state.categoriesReducer)
-    const [modalType, setModalType] = useState('')
-    const [currentCategory, setCurrentCategory] = useState({})
-    const [search, setSearch] = useState('')
-    const { t } = useTranslation()
-    const dataSource = [];
-    const [refresh, setRefresh] = useState(false)
+  const [open, setOpen] = useState()
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
+  const { categories, categoriesFetchingStatus } = useSelector(state => state.categoriesReducer)
+  const [modalType, setModalType] = useState('')
+  const [currentCategory, setCurrentCategory] = useState({})
+  const [search, setSearch] = useState('')
+  const { t } = useTranslation()
+  const dataSource = [];
+  const [refresh, setRefresh] = useState(false)
 
 
-    const [categoriesIsOpen, setCategoriesIsOpen] = useState(false);
-    const [categoryDetails, setCategoryDetails] = useState({});
-
-
-    const deleteCategoryFunc = (id) => {
-      Swal.fire({
-        title: t('delete'), 
-        icon: 'warning',
-        showCancelButton: true, 
-        confirmButtonText: t('yes'),
-        cancelButtonText: t('no')
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: t('deleted'),
-            icon: 'success'
-          })
-          dispatch(fetchingCategories())
-          axios.delete(`${URL}/api/category/${id}`, setToken())
+  const [categoriesIsOpen, setCategoriesIsOpen] = useState(false);
+  const [categoryDetails, setCategoryDetails] = useState({});
+  const deleteCategoryFunc = (id) => {
+    Swal.fire({
+      title: t('delete'),
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: t('yes'),
+      cancelButtonText: t('no')
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: t('deleted'),
+          icon: 'success'
+        })
+        dispatch(fetchingCategories())
+        axios.delete(`${URL}/api/category/${id}`, setToken())
           .then(res => {
             dispatch(deleteCategory(id))
             setRefresh(!refresh)
           })
-        }
-      })
-      
+      }
+    })
+
+  }
+
+
+  categories?.map(item => {
+    dataSource.push({
+      key: item.id,
+      name: <div className="table-title"> <h2>{ item.name }</h2></div>,
+      whole_percent: item.whole_percent,
+      min_percent: item.min_percent,
+      max_percent: item.max_percent,
+      min_product: item.min_product,
+      action: (
+        <div className='table-button__group'>
+          <Button className="table-action" onClick={ () => {
+            setCurrentCategory(prev => item)
+            setModalType('update')
+            setOpen(!open)
+          } }><i className="bx bx-edit"></i></Button>
+          <Button className="table-action" onClick={ e => deleteCategoryFunc(item.id) }><i className="bx bx-trash"></i></Button>
+        </div>
+      )
+    })
+
+  })
+
+
+
+
+  const columns = [
+    {
+      title: t('categories'),
+      dataIndex: 'name',
+      key: 'key',
+    },
+
+    {
+      title: t('ulgurji_foiz_text'),
+      dataIndex: 'whole_percent',
+      key: 'key'
+    },
+    {
+      title: t('minimum_foiz_text'),
+      dataIndex: 'min_percent',
+      key: 'key'
+    },
+
+    {
+      title: t('maksumum_foiz_text'),
+      dataIndex: 'max_percent',
+      key: 'key'
+    },
+
+
+    {
+      title: t('minimum_mahsulot'),
+      dataIndex: 'min_product',
+      key: 'key'
+    },
+
+    {
+      title: t('action'),
+      dataIndex: 'action',
+      key: 'key'
+    }
+  ];
+
+
+
+  useEffect(async () => {
+    dispatch(fetchingCategories())
+
+    const res = await axios.get(`${URL}/api/categories?search=${search}`, setToken());
+    setLoading(true)
+    if (res.status === 200) {
+      dispatch(fetchedCategories(res.data.payload))
+      setLoading(false)
+    } else {
+      dispatch(fetchingErrorCategories())
     }
 
-
-    categories?.map(item => {
-        dataSource.push({
-          key: item.id,
-          name: <div className="table-title"> <h2>{item.name}</h2></div>,
-          whole_percent: item.whole_percent,
-          min_percent: item.min_percent,
-          max_percent: item.max_percent,
-          min_product: item.min_product,
-          action: (
-            <div className='table-button__group'>
-              <Button className="table-action" onClick={() => {
-                  setCurrentCategory(prev => item)
-                  setModalType('update')
-                  setOpen(!open)
-              }}><i className="bx bx-edit"></i></Button>
-              <Button className="table-action" onClick={e => deleteCategoryFunc(item.id)}><i className="bx bx-trash"></i></Button>
-            </div>
-          )
-        })
-  
-      }) 
-
- 
-
-
-    const columns = [
-        {
-          title: t('categories'),
-          dataIndex: 'name',
-          key: 'key',
-        },
-    
-        {
-          title: t('ulgurji_foiz_text'),
-          dataIndex: 'whole_percent',
-          key: 'key'
-        },
-        {
-          title: t('minimum_foiz_text'),
-          dataIndex: 'min_percent',
-          key: 'key'
-        },
-    
-        {
-          title: t('maksumum_foiz_text'),
-          dataIndex: 'max_percent',
-          key: 'key'
-        },
-    
-
-        {
-            title: t('minimum_mahsulot'),
-            dataIndex: 'min_product',
-            key: 'key'
-        },
-    
-        {
-          title: t('action'),
-          dataIndex: 'action',
-          key: 'key'
-        }
-      ];
+  }, [refresh, search, open])
 
 
 
-    useEffect(async () => {
-        dispatch(fetchingCategories())
-
-        const res = await axios.get(`${URL}/api/categories?search=${search}`, setToken());
-        setLoading(true)
-        if (res.status === 200) {
-            dispatch(fetchedCategories(res.data.payload))
-            setLoading(false)
-        } else {
-            dispatch(fetchingErrorCategories())
-        }
-        
-    }, [refresh, search, open])
-
- 
 
   return (
     <div>
       <div className="section main-page">
-      
-      <Drawer title={modalType === 'add' ? t('kategoriya_qoshish') : t('kategoriyani_yangilash')} placement="right" visible={open} onClose={() => setOpen(false)}>
-        <Content setOpen={() => setOpen()} type={modalType} currentCategory={currentCategory} />
-      </Drawer>
+
+        <Drawer title={ modalType === 'add' ? t('kategoriya_qoshish') : t('kategoriyani_yangilash') } placement="right" visible={ open } onClose={ () => setOpen(false) }>
+          <Content setOpen={ () => setOpen() } type={ modalType } currentCategory={ currentCategory } />
+        </Drawer>
 
 
 
-      {!open && categoriesIsOpen ? (
-        <Modal
-          footer={null}
-          visible={categoriesIsOpen}
-          onCancel={() => {
-            setCategoriesIsOpen(false);
-            setCategoryDetails({});
-          }}
-        >
-          <h2>{categoryDetails?.name}</h2>
-          <table className="product__details-table">
-            <tbody className="product__details-table__body">
-              <tr>
-                <td>{t("ulgurji_foiz")}</td>
-                <td>
-                  {categoryDetails?.whole_percent}
-                </td>
-              </tr>
-              <tr>
-                <td>{t("minimum_foiz")}</td>
-                <td>
-                  {categoryDetails?.min_percent}
-                </td>
-              </tr>
-              <tr>
-                <td>{t("maksumum_foiz")}</td>
-                <td>
-                  {categoryDetails?.max_percent}
-                </td>
-              </tr>
-              <tr>
-                <td>{t("minimum_mahsulot")}</td>
-                <td>
-                  {categoryDetails?.min_product}
-                </td>
-              </tr> 
-            </tbody>
-          </table> 
-        </Modal>
-      ) : null}
+        { !open && categoriesIsOpen ? (
+          <Modal
+            footer={ null }
+            visible={ categoriesIsOpen }
+            onCancel={ () => {
+              setCategoriesIsOpen(false);
+              setCategoryDetails({});
+            } }
+          >
+            <h2>{ categoryDetails?.name }</h2>
+            <table className="product__details-table">
+              <tbody className="product__details-table__body">
+                <tr>
+                  <td>{ t("ulgurji_foiz") }</td>
+                  <td>
+                    { categoryDetails?.whole_percent }
+                  </td>
+                </tr>
+                <tr>
+                  <td>{ t("minimum_foiz") }</td>
+                  <td>
+                    { categoryDetails?.min_percent }
+                  </td>
+                </tr>
+                <tr>
+                  <td>{ t("maksumum_foiz") }</td>
+                  <td>
+                    { categoryDetails?.max_percent }
+                  </td>
+                </tr>
+                <tr>
+                  <td>{ t("minimum_mahsulot") }</td>
+                  <td>
+                    { categoryDetails?.min_product }
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </Modal>
+        ) : null }
 
 
 
-      <h1 className="heading">{t('categories')}</h1>
+        <h1 className="heading">{ t('categories') }</h1>
 
-      <div className="content">
+        <div className="content">
           <div className="content-top">
-             <DebounceInput 
-              placeholder={t('kategoriya_nomi')}
-              onChange={(e) => setSearch(e.target.value)}
+            <DebounceInput
+              placeholder={ t('kategoriya_nomi') }
+              onChange={ (e) => setSearch(e.target.value) }
               className="form__input wdith_3"
-              minLength={2}
-              debounceTimeout={800}
-              style={{width: '30rem'}}
-               />
-              <Button className="btn btn-primary btn-md" onClick={() => {
-                  setOpen(true)
-                  setModalType('add')
-                  }}><i className="bx bx-plus"></i> {t('kategoriya_qoshish')}</Button>
+              minLength={ 2 }
+              debounceTimeout={ 800 }
+              style={ { width: '30rem' } }
+            />
+            <Button className="btn btn-primary btn-md" onClick={ () => {
+              setOpen(true)
+              setModalType('add')
+            } }><i className="bx bx-plus"></i> { t('kategoriya_qoshish') }</Button>
           </div>
 
           <div className="content-body">
-            <Skeleton loading={loading} active>
+            <Skeleton loading={ loading } active>
 
-          <Table 
-            className="content-table lg-table"
-            dataSource={dataSource && dataSource}
-            columns={columns} />
+              <Table
+                className="content-table lg-table"
+                dataSource={ dataSource && dataSource }
+                columns={ columns } />
 
-            
 
-<div className="responsive__table">
-              {categories &&
-                categories.length > 0 &&
-                categories.map((item, index) => {
-                  return (
-                    <div
-                      className="responsive__table-item"
-                      key={index}
-                      onClick={(e) => {
-                        !(e.target.classList.value.includes("bx")) && 
-                          setCategoriesIsOpen(true); 
+
+              <div className="responsive__table">
+                { categories &&
+                  categories.length > 0 &&
+                  categories.map((item, index) => {
+                    return (
+                      <div
+                        className="responsive__table-item"
+                        key={ index }
+                        onClick={ (e) => {
+                          !(e.target.classList.value.includes("bx")) &&
+                            setCategoriesIsOpen(true);
                           setCategoryDetails(item);
-                      }}
-                    >
-                      <div className="responsive__table-item__details-name">
-                        <h3>{item?.name}</h3>
-                      </div>
-                      
-                      <div>
+                        } }
+                      >
+                        <div className="responsive__table-item__details-name">
+                          <h3>{ item?.name }</h3>
+                        </div>
 
-                        <div className="responsive__table-item__details-actions">
-                          <div className="table-button__group">
-                          <>
-                          <Button className="table-action" onClick={() => {
-                              setCurrentCategory(prev => item)
-                              setModalType('update')
-                              setOpen(!open)
-                          }}><i className="bx bx-edit"></i></Button>
-                          <Button className="table-action" onClick={e => deleteCategoryFunc(item.id)}><i className="bx bx-trash"></i></Button>
-                        
-                          </>
+                        <div>
+
+                          <div className="responsive__table-item__details-actions">
+                            <div className="table-button__group">
+                              <>
+                                <Button className="table-action" onClick={ () => {
+                                  setCurrentCategory(prev => item)
+                                  setModalType('update')
+                                  setOpen(!open)
+                                } }><i className="bx bx-edit"></i></Button>
+                                <Button className="table-action" onClick={ e => deleteCategoryFunc(item.id) }><i className="bx bx-trash"></i></Button>
+
+                              </>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                })}
-            </div>
+                    );
+                  }) }
+              </div>
 
-       
+
             </Skeleton>
           </div>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
